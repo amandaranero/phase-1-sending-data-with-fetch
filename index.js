f810e885ecb5c2a7366296cf2fa70dedc1f80dd3
amanda ranero <amandaranero@gmail.com>
2023-02-28 09:41:46 -0500

    first attempt, not finished

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+
+function sumbitData(){
+    
+}
+
+const configObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify (sumbitData)
+}
+
+fetch("http://localhost:3000/users", configObject)
+    .then((resp)=> resp.json())
+    .then ((dogData)=> console.log(dogData));
+
+
